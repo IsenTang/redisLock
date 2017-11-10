e85aa7e55037488596e4c34f5f84d5973860a6e0
System Administrator <root@tangyichaodeMacBook-Pro.local>
2017-11-10 20:24:19 +0800

    mocha

--- redisLock.js
+++ redisLock.js
@@ -1,37 +1,63 @@
 'use strict'
 
 let RedisConnect = require('./redisConnection');
+let _ = require('lodash');
+let VError = require('verror');
 
-module.exports = getLock = () => {
-    let currentTimeStamp = new Date().getTime();
-    let result = await RedisConnect.setnx(key, currentTimeStamp);
-    if (result == 1) {
-        return true;
-    } else {
-        let currentTime = new Date().getTime();
-        let lockTime = await RedisConnect.get(key);
-        if (lockTime != null && currentTime - lockTime >= 120000) {
-            let oldValue = await RedisConnect.getset(key, currentTime);
-            if (oldValue == lockTime) {
-                //获得锁
+
+module.exports = {
+    getLock: async(key) => {
+        try {
+            let currentTimeStamp = new Date().getTime();
+            let lockName = key + '.lock';
+            let result = await RedisConnect.setnx(lockName, currentTimeStamp);
+            if (result == 1) {
                 return true;
             } else {
-                await setNx(key);
-            }
-        } else {
-            await sleep(5);
-            // console.log('errCount = ' + errCount++);
-            let result = await setNx(key);
-            return result;
+                let currentTime = new Date().getTime();
+                let lockTime = await RedisConnect.get(lockName);
+                if (lockTime != null && currentTime - lockTime >= 120000) {
+                    let oldValue = await RedisConnect.getset(lockName, currentTime);
+                    if (oldValue == lockTime) {
+                        //获得锁
+                        return true;
+                    } else {
+                        await getLock(lockName);
+                    }
+                } else {
+                    await sleep(5);
+                    // console.log('errCount = ' + errCount++);
+                    let result = await getLock(lockName);
+                    return result;
+                }
+            };
+        } catch (error) {
+            throw error;
         }
-    };
+    },
+
+    delLock: async(key) => {
+        try {
+            if (!_.isEmpty(key)) {
+                await RedisConnect.del(key + '.lock');
+                return true;
+            } else {
+                throw new VError('key is undefinded');
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
 }
+
+
+
 
 
 async function sleep(ms) {
     return new Promise((resolve, reject) => {
         setTimeout(function() {
-            resolve('');
+            resolve();
         }, ms);
     })
 }
--- redisLock.test.js
+++ redisLock.test.js
@@ -1,3 +1,21 @@
 'use strict'
 
-let redisLock = require('./redisLock');
+let redisLock = require('./redisLock').getLock;
+let delLock = require('./redisLock').delLock;
+let expect = require('chai').expect;
+
+
+describe('redisLock', function() {
+    describe('firstTime', function() {
+        it('should return true when get the lock', async function() {
+            expect(await redisLock('test')).to.be.ok;
+        });
+    });
+
+    describe('delLock', function() {
+        it('should return true when del the lock', async function() {
+            expect(await delLock('test')).to.be.ok;
+        });
+    });
+
+});
